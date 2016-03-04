6e49f9e325270e6af75be3ef8278201ee22691b1
Rohan Kapoor <rohan@rohankapoor.com>
2016-03-04 15:05:37 -0600

    Creating rest framework generic class based views to make API endpoints for listing HardDrives and viewing details

--- drivetracker/drives/views.py
+++ drivetracker/drives/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from drivetracker.drives.models import HardDrive
+from drivetracker.drives.serializers import HardDriveSerializer
+from rest_framework import generics
+
+
+class HardDriveList(generics.ListAPIView):
+    queryset = HardDrive.objects.all()
+    serializer_class = HardDriveSerializer
+
+
+class HardDriveDetail(generics.RetrieveAPIView):
+    queryset = HardDrive.objects.all()
+    serializer_class = HardDriveSerializer

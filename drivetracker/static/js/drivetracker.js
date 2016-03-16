4d16643013663fd546a76f6d6c376d23d233365f
Rohan Kapoor <rohan@rohankapoor.com>
2016-03-15 23:26:13 -0500

    Refactoring all of the d3 graphing code to smoothly transition paths within the same svg objects rather than simply replacing the whole svg and drawing everything again. Tooltips/popovers are not redrawn yet. Part of issue #2

--- drivetracker/static/js/drivetracker.js
+++ drivetracker/static/js/drivetracker.js
@@ -27,144 +27,196 @@ function formatInUse(inUse) {
   return 'Unknown';
 }
 
-function drawGraphs () {
-    (function(d3) {
-      d3.select('body').selectAll('svg').remove();
-      // 'use strict';
-      var width = 200,
-        height = 200,
-        radius = Math.min(width, height) / 2,
-        donutWidth = 50,
-        legendRectSize = 18,
-        legendSpacing = 4,
-        color = d3.scale.category20();
+// Common graphing variables
+var width = 200,
+  height = 200,
+  radius = Math.min(width, height) / 2,
+  donutWidth = 50,
+  legendRectSize = 18,
+  legendSpacing = 4,
+  color = d3.scale.category20c();
 
-      // Common graphing variables
-      var arc = d3.svg.arc()
-        .innerRadius(radius - donutWidth)
-        .outerRadius(radius);
+var arc = d3.svg.arc()
+  .innerRadius(radius - donutWidth)
+  .outerRadius(radius);
 
-      var pie = d3.layout.pie()
-        .value(function(d) { return d.count; })
-        .sort(null);
+var pie = d3.layout.pie()
+  .value(function(d) { return d.count; })
+  .sort(null);
 
-      function drawSvg(d3, selector) {
-        return d3.select(selector)
-          .append('svg')
-          .attr('width', width)
-          .attr('height', height)
-          .append('g')
-          .attr("transform", "translate(" + Math.min(width,height) / 2 + "," + Math.min(width,height) / 2 + ")");
+function drawSvg(selector) {
+  return d3.select(selector)
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr("transform", "translate(" + Math.min(width,height) / 2 + "," + Math.min(width,height) / 2 + ")");
+}
+
+function drawPath(svg, dataArray) {
+  return svg.selectAll('path')
+    .data(pie(dataArray))
+    .enter().append('path')
+      .attr('fill', function(d, i) { return color(d.data.label); })
+      .attr('d', arc)
+      .each(function(d) {this._current = d; });
+}
+
+// Maps the object to an array of objects containing labels and counts
+function mapper(value, index) {
+  return [{"label": index, "count": value}];
+}
+
+function changeDataInPath(svg, path, dataArray) {
+  // Calculate changes in existing paths and transition to them
+  svg.selectAll("path").data(pie(dataArray)).transition().duration(1000).attrTween("d", arcTween).each("end", drawTooltips);
+
+  // Add new paths
+  svg.selectAll("path")
+    .data(pie(dataArray))
+  .enter().append('path')
+    .attr('fill', function(d, i) { return color(d.data.label); })
+    .attr('d', arc)
+    .each(function(d) {this._current = d; });
+
+  // Remove unneeded paths
+  svg.selectAll("path")
+    .data(pie(dataArray)).exit().remove();
+}
+
+// Store the displayed angles in _current.
+// Then, interpolate from _current to the new angles.
+// During the transition, _current is updated in-place by d3.interpolate.
+function arcTween(a) {
+  var i = d3.interpolate(this._current, a);
+  this._current = i(1);
+  return function(t) {
+    return arc(i(t));
+  };
+}
+
+function drawTooltips() {
+  console.log(true);
+}
+
+var manufacturersSvg = drawSvg('#manufacturer-graph'),
+  capacitySvg = drawSvg('#capacity-graph'),
+  statusSvg = drawSvg('#status-graph'),
+  interfaceSvg = drawSvg('#interface-graph');
+
+var alreadyDrewPaths = false;
+  manufacturersPath = null,
+  capacityPath = null,
+  statusPath = null,
+  interfacePath = null;
+
+var popovers = [];
+
+function drawGraphs() {
+  d3.json(driveAPIUrl + '?representation=original', function(error, dataset) {
+    var manufacturers = {},
+      capacities = {},
+      statuses = {},
+      interfaces = {};
+
+    dataset.forEach(function(drive) {
+      var manufacturer = drive.manufacturer,
+        inUse = formatInUse(drive.in_use),
+        status = drive.status,
+        interface = drive.interface;
+
+      manufacturers[manufacturer] = manufacturers[manufacturer] ? manufacturers[manufacturer] + 1 : 1;
+      capacities[inUse] = capacities[inUse] ? capacities[inUse] + drive.capacity : drive.capacity;
+      statuses[status] = statuses[status] ? statuses[status] + 1 : 1;
+      interfaces[interface] = interfaces[interface] ? interfaces[interface] + 1 : 1;
+    });
+
+    manufacturers = $.map(manufacturers, mapper);
+    capacities = $.map(capacities, mapper);
+    statuses = $.map(statuses, mapper);
+    interfaces = $.map(interfaces, mapper);
+
+    // Draw the graphs
+    if (!alreadyDrewPaths) {
+      manufacturersPath = drawPath(manufacturersSvg, manufacturers);
+      capacityPath = drawPath(capacitySvg, capacities);
+      statusPath = drawPath(statusSvg, statuses);
+      interfacePath = drawPath(interfaceSvg, interfaces);
+      alreadyDrewPaths = true;
+    }
+
+    else {
+      while(popovers.length) {
+        $(popovers.pop()).popover('destroy');
       }
 
-      var manufacturersSvg = drawSvg(d3, '#manufacturer-graph'),
-        capacitySvg = drawSvg(d3, '#capacity-graph'),
-        statusSvg = drawSvg(d3, '#status-graph'),
-        interfaceSvg = drawSvg(d3, '#interface-graph');
+      changeDataInPath(manufacturersSvg, manufacturersPath, manufacturers);
+      changeDataInPath(capacitySvg, capacityPath, capacities);
+      changeDataInPath(statusSvg, statusPath, statuses);
+      changeDataInPath(interfaceSvg, interfacePath, interfaces);
+    }
 
-      d3.json(driveAPIUrl + '?representation=original', function(error, dataset) {
-        var manufacturers = {},
-          capacities = {},
-          statuses = {},
-          interfaces = {};
-
-        dataset.forEach(function(drive) {
-          var manufacturer = drive.manufacturer,
-            inUse = formatInUse(drive.in_use),
-            status = drive.status,
-            interface = drive.interface;
-
-          manufacturers[manufacturer] = manufacturers[manufacturer] ? manufacturers[manufacturer] + 1 : 1;
-          capacities[inUse] = capacities[inUse] ? capacities[inUse] + drive.capacity : drive.capacity;
-          statuses[status] = statuses[status] ? statuses[status] + 1 : 1;
-          interfaces[interface] = interfaces[interface] ? interfaces[interface] + 1 : 1;
-        });
-
-        // Maps the object to an array of objects containing labels and counts
-        function mapper(value, index) {
-          return [{"label": index, "count": value}];
-        }
-        manufacturers = $.map(manufacturers, mapper);
-        capacities = $.map(capacities, mapper);
-        statuses = $.map(statuses, mapper);
-        interfaces = $.map(interfaces, mapper);
-
-        function drawPath(svg, dataArray) {
-          return svg.selectAll('path')
-            .data(pie(dataArray))
-            .enter()
-            .append('path')
-            .attr('d', arc)
-            .attr('fill', function(d, i) {
-              return color(d.data.label);
-            });
-        }
-
-        // Draw the graphs
-        var manufacturersPath = drawPath(manufacturersSvg, manufacturers),
-          capacityPath = drawPath(capacitySvg, capacities),
-          statusPath = drawPath(statusSvg, statuses),
-          interfacePath = drawPath(interfaceSvg, interfaces);
-
-        // Add tooltips to the Manufacturer's Graph
-        manufacturersPath.each(function(d,i) {
-          var total = d3.sum(manufacturers.map(function(d) {
-            return d.count;
-          }));
-          var percent = Math.round(1000 * d.data.count / total) / 10;
-          var popover = $(manufacturersPath[0][i]).popover({
-            'container': 'body',
-            'title': d.data.label,
-            'content': percent + '% of ' + total + ' drives',
-            'trigger': 'hover focus'
-          });
-        })
-
-        // Add tooltips to the Capacity Graph
-        capacityPath.each(function(d,i) {
-          var total = d3.sum(capacities.map(function(d) {
-            return d.count;
-          }));
-          var percent = Math.round(1000 * d.data.count / total) / 10;
-          var popover = $(capacityPath[0][i]).popover({
-            'container': 'body',
-            'title': d.data.label,
-            'content': percent + '% of ' + formatBytes(total),
-            'trigger': 'hover focus'
-          });
-        })
-
-        // Add tooltips to the Capacity Graph
-        statusPath.each(function(d,i) {
-          var total = d3.sum(statuses.map(function(d) {
-            return d.count;
-          }));
-          var percent = Math.round(1000 * d.data.count / total) / 10;
-          var popover = $(statusPath[0][i]).popover({
-            'container': 'body',
-            'title': d.data.label,
-            'content': percent + '% of ' + total + ' drives',
-            'trigger': 'hover focus'
-          });
-        })
-
-        // Add tooltips to the Capacity Graph
-        interfacePath.each(function(d,i) {
-          var total = d3.sum(interfaces.map(function(d) {
-            return d.count;
-          }));
-          var percent = Math.round(1000 * d.data.count / total) / 10;
-          var popover = $(interfacePath[0][i]).popover({
-            'container': 'body',
-            'title': d.data.label,
-            'content': percent + '% of ' + total + ' drives',
-            'trigger': 'hover focus'
-          });
-        })
-
+    // Add tooltips to the Manufacturer's Graph
+    manufacturersPath.each(function(d,i) {
+      var total = d3.sum(manufacturers.map(function(d) {
+        return d.count;
+      }));
+      var percent = Math.round(1000 * d.data.count / total) / 10;
+      var popover = $(manufacturersPath[0][i]).popover({
+        'container': 'body',
+        'title': d.data.label,
+        'content': percent + '% of ' + total + ' drives',
+        'trigger': 'hover focus'
       });
-    })(window.d3);
-  }
+      popovers.push(popover);
+    });
+
+    // Add tooltips to the Capacity Graph
+    capacityPath.each(function(d,i) {
+      var total = d3.sum(capacities.map(function(d) {
+        return d.count;
+      }));
+      var percent = Math.round(1000 * d.data.count / total) / 10;
+      var popover = $(capacityPath[0][i]).popover({
+        'container': 'body',
+        'title': d.data.label,
+        'content': percent + '% of ' + formatBytes(total),
+        'trigger': 'hover focus'
+      });
+      popovers.push(popover);
+    });
+
+    // Add tooltips to the Status Graph
+    statusPath.each(function(d,i) {
+      var total = d3.sum(statuses.map(function(d) {
+        return d.count;
+      }));
+      var percent = Math.round(1000 * d.data.count / total) / 10;
+      var popover = $(statusPath[0][i]).popover({
+        'container': 'body',
+        'title': d.data.label,
+        'content': percent + '% of ' + total + ' drives',
+        'trigger': 'hover focus'
+      });
+      popovers.push(popover);
+    });
+
+    // Add tooltips to the Interface Graph
+    interfacePath.each(function(d,i) {
+      var total = d3.sum(interfaces.map(function(d) {
+        return d.count;
+      }));
+      var percent = Math.round(1000 * d.data.count / total) / 10;
+      var popover = $(interfacePath[0][i]).popover({
+        'container': 'body',
+        'title': d.data.label,
+        'content': percent + '% of ' + total + ' drives',
+        'trigger': 'hover focus'
+      });
+      popovers.push(popover);
+    });
+  });
+}
 
 var lastDriveId = null;
 

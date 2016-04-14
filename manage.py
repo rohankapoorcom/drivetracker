2cfa99c2a620bae55c39d0b581e04b3704c70505
Rohan Kapoor <rohan@rohankapoor.com>
2016-04-14 13:32:08 -0500

    Setting up a new settings file for heroku

--- manage.py
+++ manage.py
@@ -2,7 +2,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drivetracker.settings.local")
+    if 'DATABASE_URL' in os.environ:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stockportfolio.settings.heroku")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drivetracker.settings.local")
 
     from django.core.management import execute_from_command_line
 

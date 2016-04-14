a727094b4232036500d205aaf4011d7866a73f87
Rohan Kapoor <rohan@rohankapoor.com>
2016-04-14 13:33:23 -0500

    Setting up a new settings file for heroku

---
+++ drivetracker/settings/heroku.py
@@ -0,0 +1,36 @@
+"""
+Django settings for drivetracker project, example.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.9/ref/settings/
+"""
+
+from drivetracker.settings.base import *
+import dj_database_url
+
+# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+CRISPY_FAIL_SILENTLY = not DEBUG
+WHITENOISE_AUTOREFRESH = DEBUG
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+    }
+}
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
